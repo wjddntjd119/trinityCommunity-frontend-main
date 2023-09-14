4f2963b959781f186c0caddc6b72ff62991b99af
JeongWooSung <wjddntjd119@naver.com>
2023-09-14 16:38:28 +0900

    update mamement Item
    어드민이 아닌 유저용 화면 제작 시작

--- src/components/management/Management_Item.jsx
+++ src/components/management/Management_Item.jsx
@@ -7,6 +7,22 @@ function Management_Item (props){
   const [noiseLevel, setNoiseLevel] = useState("");
   const [situationStatus, setSituationStatus] = useState("");
   const [situationClass, setSituationClass] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+  
+  useEffect(() =>{
+    //어드민 값 보유 확인
+    axios.get(`/api/현재 로그인한 사람의 admin 값을 토큰으로 불러옴`)
+      .then((res)=>{
+        console.log(res.data);
+        if(res.data.isAdmin === 1){
+          setIsAdmin(true);
+        }else{
+          setIsAdmin(false);
+        }
+      }).catch((err) => {
+        console.log(err);
+      })
+  }, []);
 
   useEffect(() => {
     //데시벨 값을 가져옴
@@ -53,8 +69,8 @@ function Management_Item (props){
     }
   };
 
-  return(
-    <div className="Management_Item">
+  return isAdmin ?(
+      <div className="Management_Item">
       <div className="room_number">{data.ho}호</div>
       <div className="right">
         <div className="top">
@@ -70,7 +86,12 @@ function Management_Item (props){
         </div>
       </div>
   </div>
+    
+  ):(
+    <div className="Management_Item">
+
+    </div>
   )
-}
+};
 
 export default Management_Item;
