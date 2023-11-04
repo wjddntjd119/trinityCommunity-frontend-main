300ec2cd46b77181db779808c98d78858d2a9163
Jeong WooSung <wjddntjd119@naver.com>
2023-11-04 14:58:44 +0900

    modify error
    부족한 부분 수정

--- src/AxiosController.js
+++ src/AxiosController.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true; //axios
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://dorandoran-alb-2141553575.ap-northeast-2.elb.amazonaws.com';
 
 export default axios;
